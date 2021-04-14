8a7bbf2682e35c7e684d985860db166fb64b6694
Shariq Ali Khan <38683828+shariqkhanyousufzai@users.noreply.github.com>
2021-04-14 18:01:37 +0500

    set the layout of messages
    set the layout of messages

--- application/views/user/messages.php
+++ application/views/user/messages.php
@@ -1,6 +1,6 @@
 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
 <!--begin::Subheader-->
-<div class="subheader py-6 py-lg-12 subheader-transparent" id="kt_subheader">
+<div class="subheader py-lg-4 subheader-transparent" id="kt_subheader">
 <div class="container d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
 <!--begin::Info-->
 <div class="d-flex align-items-center flex-wrap mr-1">
@@ -24,21 +24,7 @@
 <!--begin::Toolbar-->
 <div class="d-flex align-items-center">
 <!--begin::Dropdown-->
-<div class="dropdown dropdown-inline" data-toggle="tooltip" title="Quick actions" data-placement="top">
-<a href="#" class="btn btn-fixed-height btn-bg-white btn-text-dark-50 btn-hover-text-primary btn-icon-primary font-weight-bolder font-size-sm px-5 mr-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-<span class="svg-icon svg-icon-md">
-<!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Add-user.svg-->
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-<polygon points="0 0 24 0 24 24 0 24" />
-<path d="M18,8 L16,8 C15.4477153,8 15,7.55228475 15,7 C15,6.44771525 15.4477153,6 16,6 L18,6 L18,4 C18,3.44771525 18.4477153,3 19,3 C19.5522847,3 20,3.44771525 20,4 L20,6 L22,6 C22.5522847,6 23,6.44771525 23,7 C23,7.55228475 22.5522847,8 22,8 L20,8 L20,10 C20,10.5522847 19.5522847,11 19,11 C18.4477153,11 18,10.5522847 18,10 L18,8 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-<path d="M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
-</g>
-</svg>
-<!--end::Svg Icon-->
-</span>Compose</a>
 
-</div>
 <!--end::Dropdown-->
 <!--begin::Dropdown-->
 
@@ -58,7 +44,7 @@
 
 <!--end::Sidebar-->
 <!--begin::List-->
-<div class="flex-row-fluid ml-lg-12 d-block" id="kt_inbox_list">
+<div class="flex-row-fluid  d-block" id="kt_inbox_list">
 <!--begin::Card-->
 <div class="card card-custom card-stretch">
 <!--begin::Header-->
